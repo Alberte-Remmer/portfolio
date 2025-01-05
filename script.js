a8908f8dc453bb23523cba002ad01a466dc78808
Alberte-Remmer <alre0003@stud.kea.dk>
2025-01-05 21:07:28 +0100

    skitse_done

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function menuClick() {
   burger.classList.remove("active");
   nav.classList.remove("active");
 }
+
+//Slideshow
+window.onload = function () {
+  let slideIndex = 0;
+
+  function showSlides() {
+    const slides = document.getElementsByClassName("slide"); // Henter alle billederne
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; // Skjuler alle billeder
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1; // Hvis vi er kommet til slutningen, starter vi fra første billede
+    }
+    slides[slideIndex - 1].style.display = "block"; // Viser det næste billede
+  }
+
+  setInterval(showSlides, 5000); // Skifter billede hver 3. sekund
+};
